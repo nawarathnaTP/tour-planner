1d2fbb2a5aa0dbfaa87e9b746646047db944ac04
Praveen Nawarathna <tpn.142@gmail.com>
2026-01-14 15:51:38 +0530

    Initial springboot commit

---
+++ tour-planner/src/test/java/io/github/nawarathnaTP/tour_planner/SmartTourPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nawarathnaTP.tour_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTourPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
