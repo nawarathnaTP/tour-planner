b5fe5e46f9db9124e05dfdb4ebb0c3efff873034
Praveen Nawarathna <tpn.142@gmail.com>
2026-01-27 19:03:16 +0530

    The base class is done

--- tour-planner/src/main/java/io/github/nawarathnaTP/tour_planner/Model/BaseEntity.java
+++ tour-planner/src/main/java/io/github/nawarathnaTP/tour_planner/Model/BaseEntity.java
@@ -1,4 +1,30 @@
 package io.github.nawarathnaTP.tour_planner.Model;
 
-public class BaseEntity {
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+import java.io.Serializable;
+
+/**
+This is the base of every class that will be created
+Removes repetition of id in every class
+Makes every class serializable
+ **/
+
+@MappedSuperclass
+public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public boolean isNew(Integer id){
+        return this.id == null;
+    }
 }
